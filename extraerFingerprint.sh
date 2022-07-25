ea03586657418bdf3ec64148d47a1dbfd5e80d8e
blackalv <68337752+blackalv@users.noreply.github.com>
2022-07-25 13:36:10 -0500

    Update extraerFingerprint.sh

--- extraerFingerprint.sh
+++ extraerFingerprint.sh
@@ -1,4 +1,4 @@
-##Autor blackalv##
+##Autor bl4ck4lv##
 ##Fecha: 05/10/2021##
 ##Script para obtener los fingerprint de los certificados ssl y con esto automatizar el monitoreo de suplantación de los certificados digitales de los sitios institucionales, solicitado en la normativa seps 103 artículo 20 literal f.
 
